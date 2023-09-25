aa033d8e90c23e4f38dd1ccd104e530c0b86fede
Nathan <kiprotichnathan@yahoo.com>
2023-09-25 11:42:15 +0300

    Task done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate')
+{
+    return 'this Saturday, I want to ${activity}!';
+
+}
+function mondayWork(work = 'go to thr office')
+{
+    return 'This is Monday,I will ${work}.';
+
+}
+function WrapAdjective(wrap = '*')
+{
+    return function(result = 'a hard worker'){
+        return 'You are ${wrap}${result}${wrap}!';
+
+    };
+}
